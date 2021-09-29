ed7b5c8df0d4f52f1a3d98330a5f048bf31a56e2
GustavoPeredo <gustavomperedo@protonmail.com>
2021-09-29 23:37:37 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,233 @@
+use std::collections::HashMap;
+use std::fs::{self, create_dir_all};
+use std::path::PathBuf;
+use std::process::Command;
+use std::str;
+
+use dirs::data_dir;
+
+use serde_json;
+use toml;
+
+mod repo;
+
+fn get_share_dir() -> PathBuf {
+    let mut share = match data_dir() {
+        Some(p) => p,
+        _ => {
+            println!("Couldn't solve for dir, using current");
+            PathBuf::from("./")
+        }
+    };
+    share.push("font-catcher");
+    create_dir_all(&share).expect("Couldn't create direcotires!");
+    share
+}
+
+fn get_local_repos(repo_file: PathBuf) -> repo::Repositories {
+    let repo_data = fs::read_to_string(&repo_file).expect("Unable to read repositories file");
+    toml::from_str(&repo_data).expect("Failed to read repositories file")
+}
+
+fn get_repo_json(url: &str) -> String {
+    if cfg!(target_os = "windows") {
+        return "".to_string();
+    }
+    match str::from_utf8(
+        &Command::new("curl")
+            .arg(url)
+            .output()
+            .expect("Failed to execute process")
+            .stdout,
+    ) {
+        Ok(v) => v.to_string(),
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    }
+}
+
+fn update_repos(repos: repo::Repositories, repos_dir: &PathBuf) {
+    create_dir_all(repos_dir).expect("Couldn't create direcotires!");
+    for repo in repos.repo.iter() {
+        println!("Updating {}...", repo.name);
+        fs::write(
+            repos_dir.join(format!("{}{}", &repo.name, ".json")),
+            match &repo.key {
+                Some(key) => get_repo_json(&repo.url.replace("{API_KEY}", &key)),
+                _ => get_repo_json(&repo.url),
+            },
+        )
+        .expect("Unable to write repo files");
+    }
+}
+
+fn family_in_repo(repos_dir: &PathBuf, repo: &str, search_string: &str) -> Vec<String> {
+    let fonts_as_string =
+        fs::read_to_string(repos_dir.join(&repo)).expect("Couldn't find specified repository");
+
+    let mut results: Vec<String> = Vec::new();
+
+    let fonts: repo::FontsList =
+        serde_json::from_str(&fonts_as_string).expect("Repository file bad formatted");
+    for font in fonts.items.iter() {
+        if font.family.contains(&search_string) {
+            results.push(font.family.to_owned());
+        }
+    }
+    results
+}
+
+fn print_results(repo: &str, fonts: &Vec<String>) {
+    if fonts.len() > 0 {
+        for i in fonts.iter() {
+            println!("{}/{}", &repo, &i);
+        }
+    } else {
+        println!("No results found!");
+    }
+}
+
+fn search_fonts(repos_dir: &PathBuf, repo: Option<&str>, search_string: &str) {
+    match &repo {
+        Some(repo) => {
+            let results =
+                family_in_repo(&repos_dir, &format!("{}{}", &repo, ".json"), search_string);
+            print_results(&repo, &results);
+        }
+        _ => {
+            let files = fs::read_dir(&repos_dir).expect("Folder not available");
+            for repo in files {
+                let results = family_in_repo(
+                    &repos_dir,
+                    &repo.as_ref().unwrap().file_name().to_str().unwrap(),
+                    search_string,
+                );
+                print_results(
+                    &repo
+                        .as_ref()
+                        .unwrap()
+                        .path()
+                        .file_stem()
+                        .unwrap()
+                        .to_str()
+                        .unwrap(),
+                    &results,
+                );
+            }
+        }
+    };
+}
+
+fn download_file(output_file: &PathBuf, url: &str) -> Result<std::process::Output, std::io::Error> {
+    create_dir_all(output_file.parent().unwrap()).expect("Couldn't create direcotires!");
+    println!(
+        "Downloading to {} from {}...",
+        output_file.as_os_str().to_str().unwrap(),
+        url
+    );
+    Command::new("curl")
+        .arg("-L") //Follow url
+        .arg("-o")
+        .arg(output_file.as_os_str().to_str().unwrap())
+        .arg(url)
+        .output()
+}
+
+fn files_in_repo(repos_dir: &PathBuf, repo: &str, search_string: &str) -> HashMap<String, String> {
+    let fonts_as_string =
+        fs::read_to_string(repos_dir.join(&repo)).expect("Couldn't find specified repository");
+
+    let mut results: HashMap<String, String> = HashMap::new();
+
+    let fonts: repo::FontsList =
+        serde_json::from_str(&fonts_as_string).expect("Repository file bad formatted");
+    for font in fonts.items.iter() {
+        if font.family == search_string {
+            for (i, j) in font.files.iter() {
+                results.insert(i.to_string(), j.to_string());
+            }
+        }
+    }
+    results
+}
+
+fn download_fonts(
+    repos_dir: &PathBuf,
+    download_dir: &PathBuf,
+    repo: Option<&str>,
+    fonts: Vec<&str>,
+) {
+    match &repo {
+        Some(repo) => {
+            for i in fonts.iter() {
+                let results = files_in_repo(&repos_dir, &format!("{}{}", &repo, ".json"), i);
+                for (variant, url) in results {
+                    let extension: &str = url
+                        .split(".")
+                        .collect::<Vec<&str>>()
+                        .last()
+                        .expect("File extension not found!");
+                    download_file(
+                        &download_dir.join(&format!("{}-{}.{}", i, &variant, extension)),
+                        &url,
+                    )
+                    .expect(&format!("Failed to download font {}", i));
+                }
+            }
+        }
+        _ => {
+            let files = fs::read_dir(&repos_dir).expect("Folder not available");
+            for repo in files {
+                for i in fonts.iter() {
+                    let results = files_in_repo(
+                        &repos_dir,
+                        &repo.as_ref().unwrap().file_name().to_str().unwrap(),
+                        i,
+                    );
+                    // Make this threaded
+                    for (variant, url) in results {
+                        let extension: &str = url
+                            .split(".")
+                            .collect::<Vec<&str>>()
+                            .last()
+                            .expect("File extension not found!");
+                        download_file(
+                            &download_dir.join(&format!("{}-{}.{}", i, &variant, extension)),
+                            &url,
+                        )
+                        .expect(&format!("Failed to download font {}", i));
+                    }
+                }
+            }
+        }
+    };
+}
+
+fn main() {
+    let share_dir = get_share_dir();
+
+    let repos_dir = share_dir.join("repos");
+    let install_dir = share_dir.join("fonts");
+
+    let repos_file = share_dir.join("repos.conf");
+
+    let default_repos: repo::Repositories = repo::get_default_repos();
+    let local_repos: repo::Repositories = get_local_repos(repos_file);
+
+    update_repos(default_repos, &repos_dir);
+    update_repos(local_repos, &repos_dir);
+
+    search_fonts(&repos_dir, None, "Agave");
+    download_fonts(&repos_dir, &install_dir, None, vec!["Agave"]);
+    download_fonts(
+        &repos_dir,
+        &install_dir,
+        None,
+        vec!["Roboto", "Hack", "ABeeZee"],
+    );
+    /*
+    Create repo file from default repositories
+
+    let repos_str = toml::to_string(&repos).unwrap();
+    fs::write(config_file, repos_str).expect("Unable to write file");
+    */
+}
---
+++ src/repo.rs
@@ -0,0 +1,54 @@
+use std::collections::HashMap;
+use std::env::var;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Font {
+    pub kind: Option<String>,
+    pub family: String,
+    pub variants: Vec<String>,
+    pub subsets: Option<Vec<String>>,
+    pub version: Option<String>,
+    pub last_modified: Option<String>,
+    pub files: HashMap<String, String>,
+    pub commentary: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FontsList {
+    pub kind: String,
+    pub items: Vec<Font>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Repository {
+    pub name: String,
+    pub url: String,
+    pub key: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Repositories {
+    pub repo: Vec<Repository>,
+}
+
+pub fn get_default_repos() -> Repositories {
+    let repos: Repositories = Repositories {
+        repo: vec![
+            #[cfg(feature = "google_repo")]
+            Repository {
+                name: "Google Fonts".to_string(),
+                url: "https://www.googleapis.com/webfonts/v1/webfonts?key={API_KEY}".to_string(),
+                key: Some(var("GOOGLE_FONTS_KEY").unwrap()),
+            },
+
+            Repository {
+                name: "Open Font Repository".to_string(),
+                url: "https://raw.githubusercontent.com/GustavoPeredo/open-font-repository/main/fonts.json".to_string(),
+                key: None,
+            }
+        ],
+    };
+    repos
+}
