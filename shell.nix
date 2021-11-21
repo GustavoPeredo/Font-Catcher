13d469a8fe93ff32793911cc7947ec1633560e03
Gustavoperedo <gustavomperedo@protonmail.com>
2021-11-21 13:55:23 +0100

    Return to nix-shell (wait for servo's nix to be updated

---
+++ shell.nix
@@ -0,0 +1,44 @@
+{ pkgs ? import <nixpkgs> {
+  overlays = [ (self: super: {
+    neovim = super.neovim.override {
+      viAlias = true;
+      vimAlias = true;
+      configure = {
+        packages.myVimPackage = with pkgs.vimPlugins; {
+          start = [ vim-nix coc-nvim coc-rls ];
+        };
+	customRC = ''
+	  set ignorecase
+	  set mouse=v
+	  set hlsearch
+	  set number
+	  set cc=80
+	  filetype plugin indent on
+	  set ttyfast
+          highlight ColorColumn ctermbg=7
+        '';
+      };
+    };
+  })];
+} }:
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [ 
+      rustc
+      rust-analyzer
+      rls
+      cargo
+
+      pkg-config
+      freetype
+      openssl
+      cmake 
+      llvm
+      gnumake
+      expat
+      fontconfig
+
+      neovim
+      nodejs
+      rustup
+    ];
+}
